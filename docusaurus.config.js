486b49de237b8a01eaa4a0ba23268be352b1923d
labpa <pbienenstein@posteo.de>
2023-06-28 09:50:29 +0000

    testing env

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://labpa.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docusaurus/',
+  baseUrl: process.env.IS_LOCAL ? '/' : '/docusaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
