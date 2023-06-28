d9015e23d1e5cbc3fb972beb49122398957c4c8f
labpa <pbienenstein@posteo.de>
2023-06-28 12:31:25 +0000

    edit favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://labpa.github.io',
-  staticDirectories: ['static'],
+  //staticDirectories: ['static'],
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: process.env.IS_LOCAL ? '/' : '/docusaurus/',
@@ -61,10 +61,10 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'Mitschriften',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
@@ -93,25 +93,9 @@ const config = {
               },
             ],
           },
+
           {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
+            title: 'Github',
             items: [
               //{
                 //label: 'Blog',
@@ -119,12 +103,12 @@ const config = {
               //},
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/labpa/mitschrift',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Labpa`,
       },
       prism: {
         theme: lightCodeTheme,
